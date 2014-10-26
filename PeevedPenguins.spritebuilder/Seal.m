3bf744f5eb5d8ff5497a4442504b058c0f98ea8a
Stella Mao <stmeow@users.noreply.github.com>
2014-10-25 17:39:43 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Seal.m
+++ PeevedPenguins.spritebuilder/Seal.m
@@ -9,15 +9,15 @@
 #import "Seal.h"
 
 @implementation Seal
-- (id)init {
-    self = [super init];
-    
-    if (self) {
-        CCLOG(@"Seal created");
-    }
-    
-    return self;
-}
+//- (id)init {
+//    self = [super init];
+//    
+//    if (self) {
+//        CCLOG(@"Seal created");
+//    }
+//    
+//    return self;
+//}
 
 -(void) didLoadFromCCB
 {
